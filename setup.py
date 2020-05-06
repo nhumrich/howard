b0130ebf7d5ee5b54cc5ffb43adf49b5c9d6c152
Nick Humrich <nick@humrich.us>
2020-05-06 13:20:07 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='howard',
-    version='1.2.0',
+    version='1.4.0',
     packages=['howard'],
     long_description=long_description,
     url='https://github.com/nhumrich/howard',
